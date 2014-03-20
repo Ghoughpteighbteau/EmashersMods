76a75eff18e5276ff55e7a524d354090efd62e87
Alex Bruce <Alex@spirewireless.com>
2014-03-19 18:56:53 -0700

    added limestone to oreDict

--- main/scala/emasher/core/EmasherCore.java
+++ main/scala/emasher/core/EmasherCore.java
@@ -361,6 +361,7 @@ public class EmasherCore
 		
 		GameRegistry.registerBlock(limestone, "limestone");
 		LanguageRegistry.addName(limestone, "Limestone");
+        OreDictionary.registerOre("blockLimestone", new ItemStack(limestone));
 		
 		normalCube = (new BlockNormalCube(normalCubeID, 0, Material.rock))
 				.setHardness(1.5F).setStepSound(Block.soundStoneFootstep)
